3fd212f721300d2f4eb41f1152208047471b0457
LilSaint <pamela.carric@gmail.com>
2017-02-13 16:23:14 +0100

    Ajout de la class GridControl, qui gère la grille du jeu

--- script/GridControl.js
+++ script/GridControl.js
@@ -1,4 +1,4 @@
-/*global console, $, jQuery, document, window */
+/*global console, $, jQuery, document, window, Tuile */
 
 function log(D) {
     'use strict';
@@ -7,26 +7,24 @@ function log(D) {
 
 /**
  * Gestion du système de grille du jeu
- * @param {[[Type]]} size          [[Description]]
- * @param {[[Type]]} previousState [[Description]]
+ * @param {int} size          [[Description]]
+ * @param {bool} previousState [[Description]]
  */
 function GridControl(size, previousState) {
     'use strict';
-    console.log('Je suis le Grid Control');
+    log('Je suis le Grid Control');
     this.size = size;
     this.cells = previousState ? this.fromState(previousState) : this.empty();
-
-    //    console.log(this.cells);
 }
 
 /**
- * Fonction qui créé la grille du jeu par rapport à une taille donnée
+ * Fonction qui créé la grille du jeu par rapport à une taille donnée (quand la grille est vide)
  * @returns {array} [Les cellules du jeu]
  */
 GridControl.prototype.empty = function () {
     'use strict';
     //Déclaration du tableau de cellule
-    var cells = ['prout'],
+    var cells = [],
         x,
         y,
         row;
@@ -34,12 +32,45 @@ GridControl.prototype.empty = function () {
     for (x = 0; x < 5; x += 1) {
         // Chaque ligne sur l'axe des x contient une cellule du tableau
         row = cells[x] = [];
-        log(row);
+        //        log(row);
 
         for (y = 0; y < this.size; y += 1) {
             row.push(null);
         }
     }
-    log(cells);
+    //    log(cells);
     return cells;
+};
+
+/**
+ * [[Description]]
+ * @param   {[[Type]]} state [[Description]]
+ * @returns {[[Type]]} [[Description]]
+ */
+GridControl.prototype.fromState = function (state) {
+    'use strict';
+    log('Je suis dans fromState');
+
+    var cells = [],
+        x,
+        y,
+        row,
+        tuile,
+        tile;
+
+    for (x = 0; x < 5; x += 1) {
+        row = cells[x] = [];
+        log(row);
+
+        for (y = 0; y < this.size; y += 1) {
+            tuile = state[x][y];
+            log(tuile);
+            if (tuile) {
+                tile = new Tuile(tuile.position, tuile.value);
+                log(tile);
+            } else {
+                return null;
+            }
+        }
+    }
 };
