866c4ff1ee2b74c265ebbc4af9b8a7e19e1bcf31
LilSaint <pamela.carric@gmail.com>
2017-02-13 16:25:21 +0100

    Ajout de la class HTMLControl qui gère toute l'interface du jeu du côté front

--- script/HTMLControl.js
+++ script/HTMLControl.js
@@ -1,5 +1,20 @@
 /*global console, $, jQuery, document, window */
+/**
+ * Gère l'interface du jeu du côté front
+ */
 function HTMLControl() {
     'use strict';
+
+    function log(D) {
+        console.log(D);
+    }
     console.log('Je suis le HTML Control');
+
+    this.tuileContainer = $('.tuile-container');
+    this.scoreContainer = $('.score-container');
+    this.bestContainer = $('.best-container');
+
+    log('TuileContainer');
+    log(this.tuileContainer);
+    log(this.bestContainer);
 }
