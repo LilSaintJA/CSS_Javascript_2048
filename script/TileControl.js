82aa3dcaa53919df639552afad1c4d454389fe55
LilSaint <pamela.carric@gmail.com>
2017-02-13 18:13:33 +0100

    Class qui gère les cases

---
+++ script/TileControl.js
@@ -0,0 +1,15 @@
+/*global console, $, jQuery, document, window, GridControl, Grid */
+
+/**
+ * Fonction de débug
+ * @param {data} D [La data à débuguer]
+ */
+function log(D) {
+    'use strict';
+    console.log(D);
+}
+
+function TileControl(position, value) {
+    'use strict';
+    log('Je suis le TileControl');
+}
