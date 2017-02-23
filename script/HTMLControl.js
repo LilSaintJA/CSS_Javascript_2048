2d8bf6fd1d4909b8aa237110070dcba60c911fec
LilSaint <pamela.carric@gmail.com>
2017-02-23 07:41:27 +0100

    Debug class des tiles

--- script/HTMLControl.js
+++ script/HTMLControl.js
@@ -15,6 +15,7 @@ function HTMLControl() {
     'use strict';
     log('Je suis le HTML Control');
 
+    // Selecteur de div nécessaire pour le jeu
     this.tuileContainer     = $('.tuile-container');
     this.scoreContainer     = $('.score-container');
     this.bestContainer      = $('.best-container');
@@ -22,7 +23,52 @@ function HTMLControl() {
 
     this.score = 0;
 
+    this.addTile(this);
+
     //    log('TuileContainer');
     //    log(this.tuileContainer);
     //    log(this.bestContainer);
 }
+
+// *** Ajout de méthode à HTMLControl
+
+/**
+ * Gère l'ajout d'une tile dans la grille HTML
+ * @param {[[Type]]} tile [[Description]]
+ */
+HTMLControl.prototype.addTile = function (tile) {
+    'use strict';
+
+    // self garde une référence vers "this" en cas de changement de scope
+    var self = this,
+        tileWrapper = $('<div></div>'),
+        tileInner = $('<div></div>'),
+        position = tile.previousPosition || { x: tile.x, y: tile.y },
+        positionClass = this.positionClass(position);
+    //    log('Objet courant');
+    //    log(self);
+
+    // Création des divs pour les tiles
+};
+
+/**
+ * [[Description]]
+ * @param   {object} position [[Description]]
+ * @returns {object} [[Description]]
+ */
+HTMLControl.prototype.normalizeClass = function (position) {
+    'use strict';
+    log({ x: position.x + 1, y: position.y + 1 });
+    return { x: position.x + 1, y: position.y + 1 };
+};
+
+/**
+ * [[Description]]
+ * @param {sring} position [Le nom de la class avec la position de x et y]
+ */
+HTMLControl.prototype.positionClass = function (position) {
+    'use strict';
+    position = this.normalizeClass(position);
+    log('tile-position-' + position.x + '-' + position.y);
+    return 'tile-position-' + position.x + '-' + position.y;
+};
